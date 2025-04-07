b88d83d2ffd9d1bcc01a053220b20e9d446e2df6
Irvan pramana <darknet1255@gmail.com>
2025-04-07 18:30:59 +0700

    feat: add role column to users table

--- database/migrations/000001_create_users_table.up.sql
+++ database/migrations/000001_create_users_table.up.sql
@@ -1,9 +1,11 @@
+-- Active: 1743762950190@@127.0.0.1@3306@url_shortener
 CREATE table
     users (
         id INT PRIMARY KEY AUTO_INCREMENT,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
+        role ENUM('admin', 'user') DEFAULT 'user',
         verified_email BOOLEAN DEFAULT FALSE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
