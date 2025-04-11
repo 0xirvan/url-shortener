5c7fe493c5b7a362ad86e98f397e5a6c217634f9
0xirvan <darknet1255@gmail.com>
2025-04-11 15:43:23 +0700

    fix: add VerifiedEmail field to User model for email verification status

--- model/user_model.go
+++ model/user_model.go
@@ -3,12 +3,13 @@ package model
 import "time"
 
 type User struct {
-	ID        uint      `gorm:"primaryKey;autoIncrement"`
-	Name      string    `gorm:"not null"`
-	Email     string    `gorm:"unique;not null"`
-	Password  string    `gorm:"not null"`
-	Role      string    `gorm:"default:'user';not null"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
-	Tokens    []Token   `gorm:"foreignKey:user_id;references:id"`
+	ID            uint      `gorm:"primaryKey;autoIncrement"`
+	Name          string    `gorm:"not null"`
+	Email         string    `gorm:"unique;not null"`
+	Password      string    `gorm:"not null"`
+	Role          string    `gorm:"default:'user';not null"`
+	VerifiedEmail bool      `gorm:"default:false;not null"`
+	CreatedAt     time.Time `gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
+	Tokens        []Token   `gorm:"foreignKey:user_id;references:id"`
 }
